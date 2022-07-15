1d46268e2992d62af6aca3034b54d460d31011d8
Zouhair <zouhairsahtout66@gmail.com>
2022-07-15 17:56:16 +0100

    Add book to list

--- appES6.js
+++ appES6.js
@@ -10,5 +10,46 @@ class Book {
 
 // UI Constructor
 class UI {
-
+    addBookToList(book) {
+        // Access to the list
+        const list = document.querySelector('#book-list');
+        // Create a row
+        const row = document.createElement('tr');
+        // Insert new book columns
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="delete">X</a></td>
+        `;
+        // Append the row inside my list to show up
+        list.appendChild(row);
+    }
 }
+
+// Event listener for adding a book
+const bookForm = document.querySelector('#book-form');
+bookForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    // Get the values from the inputs
+    const title  = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn   = document.querySelector('#isbn').value;
+
+    // Instantiate new book
+    const book = new Book(title, author, isbn);
+
+    // Instaniate new ui
+    const ui = new UI();
+
+    // Check if the fields are empty
+    if(title === '' || author === '' || isbn === '') {
+        alert('Fill in all fields please');
+
+    } else {
+        // Add book to list
+        ui.addBookToList(book);
+
+    }
+})
